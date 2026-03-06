c0fa83ae168d1831323a5e54f9f2230d76767e88
ilhombekk <ilhomsaidov828@mail.com>
2026-03-06 19:09:46 +0500

    delete syle

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/style/style.css"
-import "./assets/style/Calculator.css"
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
